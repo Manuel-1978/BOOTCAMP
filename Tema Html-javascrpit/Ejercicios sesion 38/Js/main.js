1cd2d1c58229a76846607b6e9af85f52f629e3f2
m@nuelrico <manuelricorios@gmail.com>
2021-04-06 22:26:13 +0200

    homework

--- Tema Html-javascrpit/Ejercicios sesion 38/Js/main.js
+++ Tema Html-javascrpit/Ejercicios sesion 38/Js/main.js
@@ -13,8 +13,8 @@ div.append(p,p2);
 
 //Apartado 2.Al pulsar un botón, cambiar el color del fondo del 
 //cuerpo de HTML
-const colorButton = document.querySelector("body");
-colorButton.addEventListener("click", function (event) {
+const colorButton = document.querySelectorAll("button")[0];
+colorButton.addEventListener("click", function (e) {
    colorButton.style.backgroundColor="red";
 });
    
@@ -35,29 +35,29 @@ listItems[2].addEventListener("click", changeImage);
 //Apartado 4:Añadir un input de tipo texto y un botón. Al pulsar el botón debe escribirse
 // el texto del input en un párrafo.
 //4.2 Añadir un nuevo input pero esta vez cambiará el texto con cada pulsación de tecla del usuario.
-const ap4Paragraph=document.getElementById("fillAbleParagraph");
+const ap4Paragraph=document.getElementById("fillableParagraph");
 const ap4Input=document.querySelector("input");
 const ap4Button=document.querySelectorAll("button")[1];
 
 ap4Button.addEventListener("click",() =>{ 
     ap4Paragraph.textContent=ap4Input.value;
-    ap4Iput.value="";
+    ap4Input.value="";
 });
 //4.2
-const ap42Paragraph=document.getElementById("fillAbleParagraph2");
+const ap42Paragraph = document.getElementById("fillAbleParagraph2");
 const ap42Input=document.querySelectorAll("input")[1];
 
-ap4Button.addEventListener("input",() => ap42Paragraph.textContent=ap4Input.value);
+ap42Input.addEventListener("input",() => ap42Paragraph.textContent =ap42Input.value);
+
 //5. De forma similar al anterior, pero para un textarea y validará 
 //si lo introducido es mayor de 15 caracteres
 const textarea = document.querySelector("#textareaToValidate");
 textarea.addEventListener("input",e =>{
 //if(e.target.value.lenght>15){
-  // e.target.style.color="red";
-  // } else{
-    //  e.target.style.color ="green";
-  // }
-  e.target.style.color = e.target.value.lenght > 15? "red":"green";
+// e.target.style.color="red";
+// } else{
+//  e.target.style.color ="green"; // }
+  e.target.style.color = e.target.value.lenght > 15 ? "red":"green";
 });
 
 //6. Añadir un input de tipo texto con leyenda: “Escribir un número par”.
@@ -65,13 +65,13 @@ textarea.addEventListener("input",e =>{
 // no. En caso negativo, cambiar los bordes del input a rojo.
 //Para revertir el estado de una propiedad, podemos utilizar el valor
 //“revert” o dejarla vacío.
-const evenInput = document.querySelector("#evenNumberInput");
+const eventInput = document.querySelector("#eventNumberInput");
 
-evenInput.nextElementSibling.addEventListener("click",()=>{
-   if(evenInput.value % 2 !== 0){
-      evenInput.style.border="2px solid red"
+eventInput.nextElementSibling.addEventListener("click",()=>{
+   if(eventInput.value % 2 !== 0){
+      eventInput.style.border="2px solid red"
   } else {
-     evenInput.style.border =""
+     eventInput.style.border ="revert"
  }});
 
 //7. Partiendo de una lista ul, crear 10 li con un texto indicando el
@@ -88,14 +88,25 @@ for (let i= 1;i<=10;i++){
 //8. Crear un enlace y un botón. Si pulso el enlace se me abre la URL 
 //en la misma página. Si pulso primero elbotón y luego el enlace, se me abre
 // en una nueva pestaña
+
 const newTabButton = document.getElementById("newTab");
 const link = document.querySelector("a");
-newTabButton.addEventListener("click",()=>link.target="_blank")
+
+newTabButton.addEventListener("click",()=>link.target="_blank");
+
 //9. Añadir un párrafo y un select con 5 opciones de colores: negro, 
 //blanco, rojo, amarillo, verde y azul. Al seleccionar un color del select, 
 //cambiar el color del párrafo.
  
-const ap9Parragraph = document.querySelector("#colorsParagraph");
+const ap9Paragraph = document.querySelector("#colorsParagraph");
 document.querySelector("#colorsSelect").addEventListener("change",e =>{
-   ap4Paragraph.style.color= e.target.value;
+   ap9Paragraph.style.color= e.target.value;
 });
+
+//10. Incluir un botón que al pulsarlo genere un número aleatorio y
+// mantenga en una lista actualiza el número de elementos que se han generado, 
+//los que son pares y los que son impares
+function numAleatorios() {
+   let numAle= parseint(Math.random()*100);
+  if (numAle %2==0){return }
+}
