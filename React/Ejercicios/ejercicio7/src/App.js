986c9f02030c51765bfec0fa310d8ed961bd95fd
m@nuelrico <manuelricorios@gmail.com>
2021-05-04 21:31:56 +0200

    home

---
+++ React/Ejercicios/ejercicio7/src/App.js
@@ -0,0 +1,14 @@
+import Footer from './Components/footer';
+import Updatefilms from './Components/updatefilms';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Updatefilms />
+       <Footer />     
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Ejercicios/ejercicio7/src/Components/updatefilms.js
@@ -0,0 +1,28 @@
+import URL_UPDATEFILMS from '../settings';
+import {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+
+export default function Updatefilms() {
+  const[details,setdetails]= useState({});
+    const {id} = useParams();
+    useEffect(() => {
+        fetch(`${URL_UPDATEFILMS}${id}`)
+        .then(response => response.json())
+        .then(data => setdetails(data));
+        
+    }, []);
+    
+    return (
+        <div className="card w-25 m-auto">
+            <div>
+            <img className="card-img-top" src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}/>
+            </div>
+             <div className="card-body">
+               <h5>{details.title}</h5>
+               <p>{details.overview}</p>
+               <date>Date: {details.release_date}</date>
+               <p>Vote average: {details.vote_average}</p>
+               </div> 
+        </div>
+    )
+}
