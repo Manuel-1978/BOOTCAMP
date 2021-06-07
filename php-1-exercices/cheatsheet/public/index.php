de8d7bb0ce3578ada2818099ff8f85dcf6a0eeb9
Manuel-1978 <https://github.com/Manuel-1978/BOOTCAMP.git>
2021-06-07 13:04:35 +0200

    20210607

--- php-1-exercices/cheatsheet/public/index.php
+++ php-1-exercices/cheatsheet/public/index.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/partial/head.php'; ?>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<?php   include("./head.php")  ?>
+
 <body>
-    <h1>Hola, Pedro</h1>
-    <p>Hoy es <strong>
-    <?php 
-    setlocale(LC_TIME, "spanish");
-    $today= getdate();
-    echo $today['l'];
+ 
+  <?php   include("./links.php")  ?>
+  
+    <?php
+    $weeekDays = [
+        1 => 'lunes',
+        2 => 'martes',
+        3 => 'miércoles',
+        4 => 'jueves',
+        5 => 'viernes',
+        6 => 'sábado',
+        7 => 'domingo',
+    ];
     ?>
-    </strong>.
-    ¿Que tal estás?</p>
+    <?php 
+       
+    ?>
+    <h1>Hola, Carlos</h1>
+    <p>Hoy es <strong><?= $weeekDays[date('N')]; ?></strong>. ¿Qué tal estás?</p>
+    <script src="/public/main.js"></script>
 </body>
+
+
 </html>
--- php-1-exercices/cheatsheet/src/ejercicios.php
+++ php-1-exercices/cheatsheet/src/ejercicios.php
@@ -27,4 +27,28 @@ function findMax(array $array):int{
 function averangeAge (array $people){
     return array_sum(array_column($people, 'age'))/count($people)
 };
-  
+
+//EjercicioReverseTest
+function reverseString (string $string): string { // $tring = Hola qué tal
+    $chars = mb_str_split($string);
+    
+    return implode('', array_reverse($chars));
+}
+
+function reverseWords (string $string): string {
+    $words = explode(' ', $string);
+
+    return implode(' ', array_reverse($words));
+}
+
+function reverseCharactersInWords (string $string): string {
+    $words = explode(' ', $string);
+
+    $turnWords = [];
+
+    foreach($words as $word) {
+        $turnWords[] = reverseString($word);
+    }
+
+    return implode(' ', $turnWords);
+}
